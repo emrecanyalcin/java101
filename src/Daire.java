9ed5cc699ce74938d2ec54b306c98b06075e69fc
Emre Can <emrecanyalcin1996@gmail.com>
2021-06-20 17:26:52 +0300

    Initial commit

--- src/Daire.java
+++ src/Daire.java
@@ -2,16 +2,20 @@ import java.util.Scanner;
 
 public class Daire {
     public static void main(String[] args) {
-        int r;
+        int r,alpha;
         double PI = 3.14;
         Scanner in = new Scanner(System.in);
-        System.out.print("Dairenin Yarıçapını Giriniz: ");
+        System.out.print("Dairenin yarıçapını giriniz: ");
         r = in.nextInt();
+        System.out.print("Merkez açı ölçüsünü giriniz: ");
+        alpha = in.nextInt();
 
         double alan = PI * r * r;
         double cevre = 2 * PI * r;
+        double alphaAlan = (PI * (r*r) * alpha) / 360;
 
         System.out.println("Dairenin Alanı: " + alan);
         System.out.println("Dairenin Çevresi: " + cevre);
+        System.out.println("Merkez Açının Alanı: " + alphaAlan);
     }
 }
